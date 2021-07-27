e80e19e67e563abb5784ddfb2b51e061e190a913
elmtree-Ahn <clfclf3138@gmail.com>
2021-07-28 08:05:40 +0900

    모달창 열고 닫기 기능 추가

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,22 +1,40 @@
 <template>
+
+  <!-- 모달창 -->
+  <div class="black-bg" v-if="모달창열렸니 == true">
+    <div class="white-bg">
+      <h4>상세 페이지</h4>
+      <p>내용을 적으세요.</p>
+      <button @click="모달창열렸니 = false">닫기</button>
+    </div>
+  </div>
+
   <!-- 메뉴 -->
   <div class="menu">
     <a v-for="(menu,i) in menus" :key="i">{{ menu }}</a>
   </div>
   <!-- 매물 -->
   <div>
-    <h4>{{ products[0] }}</h4>
-    <p>월세 {{ price1 }} 만원</p>
+    <img src="./assets/room0.jpg" class="room-img">
+    <h4 @click="모달창열렸니 = true">{{ products[0] }}</h4>
+    <p>월세 {{ price[0] }} 만원</p>
+    <button @click="신고수[0]++">허위매물신고</button>
+    <span> 신고수 : {{ 신고수[0] }}</span>
   </div>
-  <div>
+      <img src="./assets/room1.jpg" class="room-img">
+    <div>
     <h4>{{ products[1] }}</h4>
-    <p>월세 {{ price2 }} 만원</p>
+    <p>월세 {{ price[1] }} 만원</p>
+    <button @click="신고수[1]++">허위매물신고</button>
+    <span> 신고수 : {{ 신고수[1] }}</span>
   </div>
-  <div>
+      <img src="./assets/room2.jpg" class="room-img">
+    <div>
     <h4>{{ products[2] }}</h4>
-    <p>월세 {{ price3 }} 만원</p>
+    <p>월세 {{ price[2] }} 만원</p>
+    <button @click="신고수[2]++">허위매물신고</button>
+    <span> 신고수 : {{ 신고수[2] }}</span>
   </div>
-  
   
 </template>
 
@@ -27,11 +45,17 @@ export default {
   name: 'App',
   data(){
     return {
+      모달창열렸니 : false,
+      신고수 : [0, 0, 0],
       menus : ['Home', 'Shop', 'About'],
-      price1 : 160,
-      price2 : 80,
-      price3 : 50,
+      price : [160, 80, 50],
       products : ['역삼동 원룸', '천호동 원룸', '서대문 원룸']
+    }
+  },
+  methods : {
+    increase() {
+      for (let i = 0; i < 4; i++)
+      this.신고수[i]++
     }
   },
   components: {
@@ -47,6 +71,35 @@ export default {
   text-align: center;
   color: #2c3e50;
 }
+/* 모달창 */
+body {
+  margin: 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+  background: rgba(0,0,0,.5);
+}
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
+  /* <div class="black-bg">
+    <div class="white-bg">
+      <h4>상세 페이지</h4>
+      <p>내용을 적으세요.</p>
+    </div>
+  </div> */
+
+/* 메뉴바 */
 .menu {
   background: darkslateblue;
   padding: 15px;
@@ -55,6 +108,11 @@ export default {
 .menu a {
   color: white;
   padding: 10px;
+}
+/* 매물 */
+.room-img {
+  width: 100%;
+  margin-top: 40px;
 }
 </style>
 ````
