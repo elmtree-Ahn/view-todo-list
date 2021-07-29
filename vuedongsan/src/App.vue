09a851293ed665f6793301bf62e90aee3c359025
elmtree-Ahn <clfclf3138@gmail.com>
2021-07-29 22:49:43 +0900

    너무 커밋 올리고 싶어서 올린 커밋

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -32,9 +32,7 @@ export default {
       원룸들 : data,
       모달창열렸니 : false,
       신고수 : [0, 0, 0],
-      menus : ['Home', 'Shop', 'About'],
-      price : [160, 80, 50],
-      products : ['역삼동 원룸', '천호동 원룸', '서대문 원룸']
+      menus : ['Home', 'Shop', 'About']
     }
   },
   methods : {
